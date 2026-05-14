31cb25242f5f002e67ca6e302456eb6510342acf
rikifunt <rikifunt@gmail.com>
2026-05-14 08:10:22 +0200

    add e4

---
+++ esercitazione04.pl
@@ -0,0 +1,15 @@
+% Knowledge base
+
+mother(annamaria,michela).
+mother(annamaria,jacopo).
+mother(francesca,marcello).
+mother(alma,annamaria).
+mother(alma,francesca).
+mother(anna,daniele).
+
+father(daniele,michela).
+father(daniele,jacopo).
+father(sandro,marcello).
+father(eriberto,annamaria).
+father(eriberto,francesca).
+father(antonio,daniele).
